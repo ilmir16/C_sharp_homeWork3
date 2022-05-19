fadc526495ee766bfd87c88e903dc5b86dcd5fcb
ilmir <mr.ilmirka16@mail.ru>
2022-05-19 14:17:09 +0300

    Create task001 and .gitignore

---
+++ task001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string sa = Console.ReadLine();
+            int a = int.Parse(sa);
+            for(int i = 1; i <= a; i++)
+            {
+                Console.WriteLine($"Квадрат числа {i} = {i * i}");
+            }
+        }
+    }
+}
