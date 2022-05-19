2c78e681ed3cd764daa512faebef297fcc10348c
ilmir <mr.ilmirka16@mail.ru>
2022-05-19 14:27:00 +0300

    Create task002

---
+++ task002/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string sa = Console.ReadLine();
+            int a = int.Parse(sa);
+            int i = 1;
+            while(i <= a){
+                Console.WriteLine($"Куб числа {i} = {i * i * i}");
+                i++;
+            }
+        }
+    }
+}
