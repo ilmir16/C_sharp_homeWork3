5e911b6bcc68325cdb0aa26ebff9d41168b318f6
ilmir <mr.ilmirka16@mail.ru>
2022-05-19 17:22:31 +0300

    Create task007

---
+++ task007/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = new int[] {1,0,1,0,1,0,1,0};
+            for(int i = 0; i < array.Length; i++){
+                Console.WriteLine($"array[{i}] = {array[i]} ");
+            }
+        }
+    }
+}
