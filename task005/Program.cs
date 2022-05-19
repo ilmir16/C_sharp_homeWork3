ece4788768e896783273721c0898f9f0719da9ea
ilmir <mr.ilmirka16@mail.ru>
2022-05-19 16:47:41 +0300

    Create task005

---
+++ task005/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string sa = Console.ReadLine();
+            int a = int.Parse(sa);
+            int len = sa.Length;
+            int [] array = new int[len];
+            int sum = 0;
+            for(int i = 0; i <= len - 1; i++){
+                array[i] = a % 10;
+                sum = sum + array[i];
+                a = a / 10;
+            }
+            Console.WriteLine($"Сумма числа {sa} = {sum}");
+        }
+    }
+}
