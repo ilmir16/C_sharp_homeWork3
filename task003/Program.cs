b0d370002a54b2fdcc92ef2ba0e89711b222e77a
ilmir <mr.ilmirka16@mail.ru>
2022-05-19 15:07:05 +0300

    Create task003

---
+++ task003/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.Write("Введите число: ");
+            string sa = Console.ReadLine();
+            int a = int.Parse(sa);
+            int sum = 0;
+            for(int i = 1; i <= a; i++){
+                sum = i + sum;
+            }
+            Console.WriteLine($"Сумма чисел от 1 до {a} = {sum}");
+        }
+    }
+}
